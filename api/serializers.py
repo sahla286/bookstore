3f559ccad2f2d9362e3227cf401e8dfde9bb4ba3
sahla <tcsahla@gmail.com>
2024-09-10 10:44:30 +0530

    third commit

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from api.models import Books
 from django.contrib.auth.models import User
 
-class ProductSerializer(serializers.Serializer):
-    name=serializers.CharField()
-    price=serializers.IntegerField()
-    description=serializers.CharField()
-    category=serializers.CharField()
-    image=serializers.ImageField(required=False,default=None)
+# class ProductSerializer(serializers.Serializer):
+#     name=serializers.CharField()
+#     price=serializers.IntegerField()
+#     description=serializers.CharField()
+#     category=serializers.CharField()
+#     image=serializers.ImageField(required=False,default=None)
 
 class ProductModelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,3 +18,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['username','first_name','last_name','email','password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**self.validated_data)
--- api/views.py
+++ api/views.py
@@ -5,13 +5,17 @@ from api.models import Books
 from api.serializers import ProductModelSerializer,UserSerializer
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class ProductViewsetView(ModelViewSet):
     serializer_class=ProductModelSerializer
     queryset=Books.objects.all()
-
+    authentication_classes =[BasicAuthentication]
+    permission_classes =[IsAuthenticated]
+    
 # custom method
     @action(methods=['GET'],detail=False)
     def categories(self,request,*args,**kw):
